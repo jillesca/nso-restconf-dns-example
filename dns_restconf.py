60ddaad9d6e7497af06ee9c1cf4db1f581d46e1e
jillesca <jillesca@cisco.com>
2023-09-26 16:02:06 +0100

    refactoring

--- dns_restconf.py
+++ dns_restconf.py
@@ -1,77 +1,145 @@
 #!/usr/bin/env python3
 
-import sys
-import itertools
 import json
 import requests
+from requests.auth import HTTPBasicAuth
 
 # import ncs
 
-AUTH = ("admin", "admin")
+USERNAME = "admin"
+PASSWORD = "admin"
 BASE_URL = "http://localhost:8080/restconf"
 
-session = requests.Session()
-session.auth = AUTH
-headers = {"Content-Type": "application/yang-data+json"}
+
+class dns_restconf:
+    def __init__(self):
+        self.create_restconf_session()
+
+    def create_restconf_session(self):
+        session = requests.session()
+        session.auth = HTTPBasicAuth(USERNAME, PASSWORD)
+        session.headers.update({"Content-Type": "application/yang-data+json"})
+        session.verify = False
+        self.session = session
+
+    def fetch_data(self, endpoint: str) -> str:
+        try:
+            response = self.session.get(BASE_URL + endpoint)
+            response.raise_for_status()
+            return response.text
+        except Exception as err:
+            print(f"An unexpected error happened: {err=}")
+
+    def post_data(self, endpoint: str, data: dict = []) -> str:
+        try:
+            response = self.session.post(BASE_URL + endpoint, json=data)
+            response.raise_for_status()
+            return response.status_code
+        except Exception as err:
+            print(f"An unexpected error happened: {err=}")
+
+    def patch_data(self, endpoint: str, data: dict = []) -> str:
+        try:
+            response = self.session.patch(BASE_URL + endpoint, json=data)
+            response.raise_for_status()
+            return response.status_code
+        except Exception as err:
+            print(f"An unexpected error happened: {err=}")
+
+    def list_devices_in_nso(self) -> str:
+        endpoint = "/data?fields=tailf-ncs:devices/device(name;address)"
+        response = self.fetch_data(endpoint)
+        print(response)
+
+    def nso_sync_from(self) -> str:
+        endpoint = "/operations/tailf-ncs:devices/sync-from"
+        response = self.post_data(endpoint)
+        print(response)
+
+    def update_dns_server(self) -> str:
+        data = {
+            "data": {
+                "tailf-ncs:devices": {
+                    "device": [
+                        {
+                            "name": "ex1",
+                            "config": {
+                                "router:sys": {
+                                    "dns": {"server": [{"address": "198.51.100.1"}]}
+                                }
+                            },
+                        }
+                    ]
+                }
+            }
+        }
+        endpoint = "/data"
+        response = self.patch_data(endpoint, data)
+        print(response)
+
+    def list_rollback_files(self) -> str:
+        endpoint = "/data/tailf-rollback:rollback-files"
+        response = self.fetch_data(endpoint)
+        print(response)
+
+    def apply_rollback_file(self) -> str:
+        data = {"input": {"id": "0"}}
+        endpoint = "/data/tailf-rollback:rollback-files/apply-rollback-file"
+        response = self.post_data(endpoint, data)
+        print(response)
+
+    # with open("packages/router/src/yang/router-dns.yang", "r", encoding="utf-8") as f:
+    #     for line in f:
+    #         if "list server" in line:
+    #             sys.stdout.write(line)
+    #             sys.stdout.writelines(itertools.islice(f, 7))
+    #             break
+
+    # with ncs.maapi.single_write_trans("admin", "python") as t:
+    #     root = ncs.maagic.get_root(t)
+    #     dns_server_list = root.devices.device["ex1"].config.sys.dns.server
+    #     dns_server_list.create("192.0.2.1")
+    #     params = t.get_params()
+    #     params.dry_run_native()
+    #     result = t.apply_params(True, params)
+    #     print(result["device"]["ex1"])
+    #     t.apply_params(True, t.get_params())
+    #     print("Done!")
+
+    def check_dns_config(self) -> str:
+        endpoint = "/data/tailf-ncs:devices/device=ex1/config/router:sys/dns/server"
+        response = self.session.get(BASE_URL + endpoint)
+        print(response)
+
+    def dry_run_dns_config(self) -> str:
+        data = {"router:server": [{"address": "192.0.2.2"}]}
+        endpoint = (
+            "/data/tailf-ncs:devices/device=ex1/config/router:sys/dns/server?dry-run"
+        )
+        response = self.patch_data(endpoint, data)
+        print(response)
+
+    def commit_dns_config(self) -> str:
+        data = {"router:server": [{"address": "192.0.2.2"}]}
+        endpoint = "/data/tailf-ncs:devices/device=ex1/config/router:sys/dns/server"
+        response = self.patch_data(endpoint, data)
+        print(response)
 
 
-PATH = "/data?fields=tailf-ncs:devices/device(name;address)"
-r = session.get(BASE_URL + PATH, headers=headers)
-print(r.text)
-
-PATH = "/operations/tailf-ncs:devices/sync-from"
-r = session.post(BASE_URL + PATH, headers=headers)
-print(r.text)
-
-SERVER_DATA = {"server": [{"address": "198.51.100.1"}]}
-SYS_DATA = {"router:sys": {"dns": SERVER_DATA}}
-DEV_DATA = {"device": [{"name": "ex1", "config": SYS_DATA}]}
-INPUT_DATA = {"data": {"tailf-ncs:devices": DEV_DATA}}
-PATH = "/data"
-r = session.patch(BASE_URL + PATH, json=INPUT_DATA, headers=headers)
-print("Status code: {}\n".format(r.status_code))
-
-PATH = "/data/tailf-rollback:rollback-files"
-r = session.get(BASE_URL + PATH, headers=headers)
-print(r.text)
-
-INPUT_DATA = {"input": {"id": "0"}}
-PATH = "/data/tailf-rollback:rollback-files/apply-rollback-file"
-r = session.post(BASE_URL + PATH, json=INPUT_DATA, headers=headers)
-print("Status code: {}\n".format(r.status_code))
-
-# with open("packages/router/src/yang/router-dns.yang", "r", encoding="utf-8") as f:
-#     for line in f:
-#         if "list server" in line:
-#             sys.stdout.write(line)
-#             sys.stdout.writelines(itertools.islice(f, 7))
-#             break
+def main() -> None:
+    restconf = dns_restconf()
+    # fetch_data()
+    restconf.list_devices_in_nso()
+    restconf.nso_sync_from()
+    restconf.check_dns_config()
+    restconf.update_dns_server()
+    restconf.list_rollback_files()
+    restconf.apply_rollback_file()
+    restconf.check_dns_config()
+    restconf.dry_run_dns_config()
+    restconf.commit_dns_config()
+    restconf.check_dns_config()
 
 
-# with ncs.maapi.single_write_trans("admin", "python") as t:
-#     root = ncs.maagic.get_root(t)
-#     dns_server_list = root.devices.device["ex1"].config.sys.dns.server
-#     dns_server_list.create("192.0.2.1")
-#     params = t.get_params()
-#     params.dry_run_native()
-#     result = t.apply_params(True, params)
-#     print(result["device"]["ex1"])
-#     t.apply_params(True, t.get_params())
-#     print("Done!")
-
-PATH = "/data/tailf-ncs:devices/device=ex1/config/router:sys/dns/server"
-r = session.get(BASE_URL + PATH, headers=headers)
-print(r.text)
-
-INPUT_DATA = {"router:server": [{"address": "192.0.2.2"}]}
-PATH = "/data/tailf-ncs:devices/device=ex1/config/router:sys/dns/server?dry-run"
-r = session.patch(BASE_URL + PATH, json=INPUT_DATA, headers=headers)
-print(json.dumps(r.json(), indent=2))
-
-PATH = "/data/tailf-ncs:devices/device=ex1/config/router:sys/dns/server"
-r = session.patch(BASE_URL + PATH, json=INPUT_DATA, headers=headers)
-print("Status code: {}\n".format(r.status_code))
-
-PATH = "/data/tailf-ncs:devices/device=ex1/config/router:sys/dns/server"
-r = session.get(BASE_URL + PATH, headers=headers)
-print(r.text)
+if __name__ == "__main__":
+    main()
