fb858667a490f2a8b45fbf8a651d24e6fec1c166
jillesca <jillesca@cisco.com>
2023-09-29 14:48:32 +0100

    refactoring print_results

--- print_terminal.py
+++ print_terminal.py
@@ -8,37 +8,41 @@ ENDC = "\033[0m"
 
 
 def print_results(data: dict) -> None:
+    print(format_data(data))
+
+
+def format_data(data: dict) -> str:
     result = ""
-    result += print_header(data.get("header", ""))
+    result += add_header(data.get("header", ""))
     result += f'Path: {data.get("path", "")}\n'
     if "data" in data:
         result += "Data sent:\n"
-        result += print_json(data.get("data", ""))
+        result += format_json(data.get("data", ""))
     result += "\nResponse:\n"
     if "body" in data:
-        result += print_blue(data.get("body", ""))
+        result += colour_blue(data.get("body", ""))
     if "json" in data:
-        result += print_json(data.get("json", ""))
-    result += print_purple(f'HTTP Method: {data.get("method", "").upper()}')
-    result += print_purple(f'HTTP Status Code: {data.get("code", "")}')
-    print(result)
+        result += format_json(data.get("json", ""))
+    result += colour_purple(f'HTTP Method: {data.get("method", "").upper()}')
+    result += colour_purple(f'HTTP Status Code: {data.get("code", "")}')
+    return result
 
 
-def print_green(msg: str) -> str:
+def colour_green(msg: str) -> str:
     return f"{OKGREEN}{msg}{ENDC}\n"
 
 
-def print_blue(msg: str) -> str:
+def colour_blue(msg: str) -> str:
     return f"{OKBLUE}{msg}{ENDC}\n"
 
 
-def print_header(msg: str) -> str:
+def add_header(msg: str) -> str:
     return f"{BOLD}{'#' * 15} {msg} {'#' * 15}{ENDC}\n"
 
 
-def print_purple(msg: str) -> str:
+def colour_purple(msg: str) -> str:
     return f"{PURPLE}{msg}{ENDC}\n"
 
 
-def print_json(msg: str) -> str:
-    return print_green(json.dumps(msg, indent=2))
+def format_json(msg: str) -> str:
+    return colour_green(json.dumps(msg, indent=2))
